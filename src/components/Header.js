2d099c460cccd449922f527bc9808e9b4e445dad
hayleyrobin <hayley.robinson63@myhunter.cuny.edu>
2025-11-17 14:34:28 -0500

    Addded on click event to button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import PropTypes from "prop-types";
+import Button from "./Button";
 
 const Header = ({ title = 'Task Tracker' }) => {
+    const onClick = () => {
+        console.log('Add Task button clicked');
+    }
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add Task" onClick={onClick} />
+
     </header>
   );
 };
