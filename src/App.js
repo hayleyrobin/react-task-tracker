486f2b9c7ce70578978482403d664a10bd5da476
hayleyrobin <hayley.robinson63@myhunter.cuny.edu>
2025-11-18 12:37:26 -0500

    added icon buttons to delete tasks

--- src/App.js
+++ src/App.js
@@ -4,10 +4,15 @@ import Tasks from "./components/Tasks";
 
 function App() {
   const [tasks, setTasks] = useState([
-    { id: 1, title: "Task 1", text: "Food Shopping", day: "Feb 5th at 2:30pm", reminder: false },
-    { id: 2, title: "Task 2", text: "Christmas Shopping", day: "Feb 6th at 1:30pm", reminder: true },
-    { id: 3, title: "Task 3", text: "Winter Cleaning", day: "Feb 7th at 3:30pm", reminder: false },
+    { id: 1, text: "Food Shopping", day: "Feb 5th at 2:30pm", reminder: false },
+    { id: 2, text: "Christmas Shopping", day: "Feb 6th at 1:30pm", reminder: true },
+    { id: 3, text: "Winter Cleaning", day: "Feb 7th at 3:30pm", reminder: false },
   ]);
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
 
   return (
     <div className="container">
