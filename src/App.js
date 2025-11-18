4e167bd572b7aaa1b8961cb5fe53d4a5d172d915
hayleyrobin <hayley.robinson63@myhunter.cuny.edu>
2025-11-18 13:08:59 -0500

    add toggle functionality for task reminders

--- src/App.js
+++ src/App.js
@@ -13,11 +13,20 @@ function App() {
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
   };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    //map through tasks, find the one with the matching id, and toggle its reminder property
+    setTasks(tasks.map((task) =>
+      task.id === id ? { ...task, reminder: !task.reminder } : task
+    ));
+  };
+
   return (
     <div className="container">
       <Header />
       {/*if tasks exist, render Tasks component, else show message*/}
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks to Show')}
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks to Show')}
     </div>
   );
 }
