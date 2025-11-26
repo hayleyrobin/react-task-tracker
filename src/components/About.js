92ebde3d28750db482922b4e1b65bd8fdac98c86
hayleyrobin <hayley.robinson63@myhunter.cuny.edu>
2025-11-26 16:37:56 -0500

    used link instead of a href tag to not reload when link is clicked

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom';
 const About = () => {
   return (
     <div>
       <h4>Version 1.0.0</h4>
       <p>This is a simple task tracker app built with React.</p>
-      <a href="/">Go Back</a>
+      <Link to="/">Go Back</Link>
     </div>
   );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,9 @@
+import {Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <footer>
       <p>Copyright &copy; 2025 Task Tracker App</p>
-      <a href="/about">About</a>
+      <Link to="/about">About</Link>
     </footer>
   );
 };
