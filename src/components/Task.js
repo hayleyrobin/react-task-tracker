5eec40c2502aa2ac4afdfe97d675b08bb4e6064e
hayleyrobin <hayley.robinson63@myhunter.cuny.edu>
2025-11-28 19:29:46 -0500

    Added complete button to show startus of task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,15 +1,17 @@
 import {FaTimes} from 'react-icons/fa';
+import { FaRegSquare } from "react-icons/fa6";
+import { FaRegSquareCheck } from "react-icons/fa6";
 import {Link} from 'react-router-dom';
 
 const Task = ({task, onDelete, onToggle, onComplete}) =>{
     return (
         <div className={`task ${task.reminder ? 'reminder' : ''} ${task.completed ? 'completed' : ''}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>{task.text}
-                <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} />
+                <FaTimes style={{ color: 'red', cursor: 'pointer' }} onClick={() => onDelete(task.id)} />     
             </h3>
-            <p>{task.day}</p>
-            <p>Completed: {task.completed ? 'Yes' : 'No'}</p>
-            <button onClick={() => onComplete(task.id)}>Complete</button>
+            <p>{`Due: ${task.day}`}</p>
+            <button className='btn' onClick={() => onComplete(task.id)}>Completed? {!task.completed ? <FaRegSquare style={{ marginLeft: '10px' }} /> : <FaRegSquareCheck style={{marginLeft: '10px'}} />}
+            </button>
             <div>
               <Link to={`/task/${task.id}`}>View Details</Link>
             </div>
